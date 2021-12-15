94f4ef3f730bacb03fbf36b05b763c373c6c4139
vitortamberlini <vitor.tamberlini@outlook.com>
2021-12-15 20:05:44 +0000

    Changed Stuff

--- src/js/app.js
+++ src/js/app.js
@@ -27,23 +27,37 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let background_display;
+  let background_image;
 
+  if (variables.includeCover == false) {
+    background_display = "none";
+  } else {
+    background_display = "";
+  }
+
+  background_image = document.querySelector("#background-image");
+
+  background_image.style.display = background_display;
+  document.querySelector("#profile-image").src = variables.avatarURL;
+  background_image.src = variables.background;
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  // document.querySelector("#that-image-id").src = variables.background;
+
+  // document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  //           ${cover}
+  //         <img src="${variables.avatarURL}" class="photo" />
+  //         <h1>Lucy Boilett</h1>
+  //         <h2>Web Developer</h2>
+  //         <h3>Miami, USA</h3>
+  //         <ul class="position-right">
+  //           <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+  //           <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+  //           <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+  //           <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+  //         </ul>
+  //       </div>
+  //   `;
 }
 
 /**
